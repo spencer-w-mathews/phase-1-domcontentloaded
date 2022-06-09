8f4928bb46eb2c44fdc91e97dcd425b888c0a5fa
Spencer Mathews <smathews0906@gmail.com>
2022-06-09 06:35:44 -0500

    commit tree 2554943cbebdd6eb8ea86071fa810600a871eb03

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("The DOM has loaded");
+    const note = document.querySelector('#text');
+    return note.textContent= 'This is really cool!';
+      
+  });
+
+ 
